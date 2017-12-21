8fb2ed65f54eb430ef7149927cea2992b0b5eb82
Cedricbob <cedric_robert27@hotmail.com>
2017-12-20 21:27:17 -0500

    Séquence de démarrage

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 
+const Port = 3000;
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
@@ -8,4 +10,5 @@ app.get('/Test', function (req, res){
     res.send('page de test');
 })
 
-app.listen(3000)
+console.log("\x1b[33m%s\x1b[0m","Server start on localhost:" + Port);
+app.listen(Port)
